b29d9d333d13bb41073d96ea6a0d20dce22b56c9
JonathanKhoo <JonathanKhoo@c4cb4a5a-a676-4c22-b279-0c4b2d07a2a5>
2011-01-25 00:52:09 +0000

    #142: Cron job to copy DFB3 files from Parkes to Epping.
    git-svn-id: https://svn.atnf.csiro.au/psrdatamanagement/trunk@30 c4cb4a5a-a676-4c22-b279-0c4b2d07a2a5

---
+++ src/ShellScripts/check_copy_dfb3_files.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#
+# Script to check whether there is a current DFB3-file transfer.
+# Starts the transfer if there isn't one running
+#
+# Author: Jonathan Khoo
+# Date:   25.01.11
+#
+
+SCRIPT_NAME="copy_dfb3_files.sh"
+SCRIPT_LOCATION="/var/www/vhosts/psrdatamanagement.atnf.csiro.au/scripts/"
+
+pgrep -f ${SCRIPT_LOCATION}${SCRIPT_NAME} > /dev/null
+
+# Exit status for pgrep:
+#   0   One or more processes matched the criteria.
+#   1   No processes matched.
+#   2   Syntax error in the command line.
+#   3   Fatal error: out of memory etc.
+
+# Run the copy_dfb3_files script, if it's not already running.
+if [ $? = 1 ]
+then
+  nohup ${SCRIPT_LOCATION}${SCRIPT_NAME} & > /dev/null
+fi
+
+exit 0
